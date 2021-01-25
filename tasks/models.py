cfc802a9de5d63dda9bbd663655435e9dd8923ac
DESKTOP-Malloraz <patryk733773@gmail.com>
2021-01-25 12:25:48 +0100

    Created 2 models and made migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    tel_number = models.IntegerField()
+
+
+class Task(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    priority = models.IntegerField(default=1)
+    description = models.CharField(max_length=100)
