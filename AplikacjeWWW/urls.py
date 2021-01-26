c6f50301fe5b12fb6db679cc544981bc3017e1e3
DESKTOP-Malloraz <patryk733773@gmail.com>
2021-01-27 00:34:07 +0100

    login and signing uo

--- AplikacjeWWW/urls.py
+++ AplikacjeWWW/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('accounts', include('accounts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 
 ]
